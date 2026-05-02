1083697541725899a1442c48823b0dc779d694e2
aroramrinaal <aroramrinaal@gmail.com>
2026-05-02 22:17:44 +0530

    chore: enable unoptimized images in next.config.ts for improved performance

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    unoptimized: true,
     domains: ['cdn.prod.website-files.com', 'xubs-akpz-sxip.n7.xano.io', 'cdn.prod.website-files.com', 'picsum.photos'],
   },
 };
