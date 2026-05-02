e475382b12871efc2b1f3db50fb808ed8e3b5f3e
aroramrinaal <aroramrinaal@gmail.com>
2026-05-02 20:45:37 +0530

    chore: remove next-sitemap configuration and related files from the project

--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://www.renvue.com',
-  generateRobotsTxt: true, // (optional) Generate a robots.txt file
-  // ...other options
-} 
